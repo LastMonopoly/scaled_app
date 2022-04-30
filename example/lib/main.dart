1bbd3518d2ff9e1da6a1a074f9f0925a8d20f744
Terminator <lastmonopoly@outlook.com>
2022-04-30 20:38:25 +0800

    Add `scaled_app` function and demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,115 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:scaled_app/scaled_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(MyApp());
+  runAppScaled(const MyApp(), baseWidth: 375); // Hot Restart after changing baseWidth
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const _title = 'Scaled app demo';
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: _title,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple, fontFamily: "Roboto"),
+      home: const MyHomePage(title: _title),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(
+        textScaleFactor: 1.1,
+        // devicePixelRatio: 10, // Provide your own value
+        // size: Size(1000, 2000), // Provide your own value
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      child: Scaffold(
+        appBar: AppBar(title: Text(title), centerTitle: false),
+        body: const TestCase(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Pressed!'),
+              duration: Duration(seconds: 1),
+            ));
+          },
+          child: const Icon(Icons.abc),
+        ),
+      ),
+    );
+  }
+}
+
+class TestCase extends StatelessWidget {
+  const TestCase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Huawei: Size(360.0, 800.0), devicePixelRatio: 3.0, w: 1080
+    // Apple: Size(375.0, 667.0), devicePixelRatio: 2.0, w: 750
+    // Pixel4: Size(411.4, 820.6), devicePixelRatio: 3.5, w: 1440
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+
+    return ListView(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Container(color: Colors.purple.shade200),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Container(color: Colors.purple.shade100),
+              ),
+            ),
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Container(color: Colors.purple.shade50),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            color: Colors.purple.shade50,
+            width: 250,
+            child: const AspectRatio(
+              aspectRatio: 1,
+              child: Center(child: Text('250 x 250')),
+            ),
+          ),
+        ),
+        Text(
+          'mediaQuery screen size is ${mediaQuery.size.width.toStringAsFixed(0)} x ${mediaQuery.size.height.toStringAsFixed(0)}\n'
+          'mediaQuery devicePixelRatio is ${mediaQuery.devicePixelRatio}\n'
+          'mediaQuery data stays the same \n',
+        ),
+      ],
     );
   }
 }
--- lib/scaled_app.dart
+++ lib/scaled_app.dart
@@ -1,7 +1,53 @@
-library scaled_app;
+import 'dart:collection';
+import 'dart:ui' show PointerDataPacket;
+import 'package:flutter/rendering.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+void runAppScaled(Widget app, {required double baseWidth}) {
+  ScaledWidgetsFlutterBinding.ensureInitialized(baseWidth)
+    ..attachRootWidget(app)
+    ..scheduleWarmUpFrame();
+}
+
+class ScaledWidgetsFlutterBinding extends WidgetsFlutterBinding {
+  final double baseWidth;
+
+  ScaledWidgetsFlutterBinding(this.baseWidth);
+
+  static WidgetsBinding ensureInitialized(double baseWidth) {
+    if (WidgetsBinding.instance == null) ScaledWidgetsFlutterBinding(baseWidth);
+    return WidgetsBinding.instance!;
+  }
+
+  @override
+  void initInstances() {
+    super.initInstances();
+    window.onPointerDataPacket = _handlePointerDataPacket;
+  }
+
+  @override
+  ViewConfiguration createViewConfiguration() {
+    return ViewConfiguration(
+      size: Size(baseWidth, window.physicalSize.height / (window.physicalSize.width / baseWidth)),
+      devicePixelRatio: window.physicalSize.width / baseWidth,
+    );
+  }
+
+  final Queue<PointerEvent> _pendingPointerEvents = Queue<PointerEvent>();
+
+  void _handlePointerDataPacket(PointerDataPacket packet) {
+    // We convert pointer data to logical pixels so that e.g. the touch slop can be
+    // defined in a device-independent manner.
+    _pendingPointerEvents
+        .addAll(PointerEventConverter.expand(packet.data, window.physicalSize.width / baseWidth));
+    if (!locked) _flushPointerEventQueue();
+  }
+
+  void _flushPointerEventQueue() {
+    assert(!locked);
+    while (_pendingPointerEvents.isNotEmpty) {
+      handlePointerEvent(_pendingPointerEvents.removeFirst());
+    }
+  }
 }
